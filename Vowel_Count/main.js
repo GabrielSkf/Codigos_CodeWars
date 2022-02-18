19fcf1547170f7269e8bd34da199952568df6169
GabrielSkf <gabrielskoferreira@gmail.com>
2022-02-18 16:29:05 -0300

    Adicionado main.js

---
+++ Vowel_Count/main.js
@@ -0,0 +1,13 @@
+/* JavaScript - Contador de Vocais */
+
+// Solucao Utilizada
+
+function getCount(str) {
+  var vowelsCount = 0;
+  for(let i = 0; i<str.length;i++){
+	if(str[i]==='a' || str[i]==='e' || str[i]==='i' || str[i]==='o' || str[i]==='u'){
+    vowelsCount++;
+	}
+  }  
+  return vowelsCount;
+}
